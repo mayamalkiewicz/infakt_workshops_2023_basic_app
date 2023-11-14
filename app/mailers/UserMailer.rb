9cf41bdc5a2e62c4ead15c20e8572e4c253e61e9
majamalkiewicz <95423761+mayamalkiewicz@users.noreply.github.com>
2023-11-14 20:00:22 +0100

    Add method to UserMailer; bug fix of interpolation

--- app/mailers/UserMailer.rb
+++ app/mailers/UserMailer.rb
@@ -4,6 +4,14 @@ class UserMailer < ApplicationMailer
     @due_date = book_loan.due_date
     @user_email = book_loan.user.email
 
-    mail(to: @user_email, subject: 'You have just loaned book #{@title} successfully.')
+    mail(to: @user_email, subject: "You have just loaned book '#{@title}' successfully.")
+  end
+
+  def loan_notification_email(book_loan)
+    @title = book_loan.book.title
+    @due_date = book_loan.due_date
+    @user_email = book_loan.user.email
+
+    mail(to: @user_email, subject: "Return date of the book '#{@title}' is coming soon - #{@due_date}.")
   end
 end
