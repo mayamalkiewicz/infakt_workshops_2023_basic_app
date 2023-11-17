50df4e1e72c6ae1f29108684d385f3e371d0140b
majamalkiewicz <95423761+mayamalkiewicz@users.noreply.github.com>
2023-11-17 17:59:35 +0100

    Add BookLoan services publishers and add it to the controller

--- app/controllers/book_loans_controller.rb
+++ app/controllers/book_loans_controller.rb
@@ -6,6 +6,7 @@ class BookLoansController < ApplicationController
     respond_to do |format|
       if @book_loan.save
         LoanCreatedJob.perform_async(@book_loan.id)
+        publish_log(@book_loan)
         format.html { redirect_to book_url(book), notice: flash_notice }
         format.json { render :show, status: :created, location: @book_loan }
       else
@@ -38,5 +39,9 @@ class BookLoansController < ApplicationController
 
   def book_loan_params
     params.require(:book_id)
+  end
+
+  def publish_log(book_loan)
+    Publishers::BookLoan.new(book_loan.attributes).publish
   end
 end
---
+++ app/services/publishers/application.rb
@@ -0,0 +1,36 @@
+require 'bunny'
+
+ module Publishers
+   class Application
+     def initialize(message:, exchange_name:, routing_key:)
+       @message = message
+       @exchange_name = exchange_name
+       @routing_key = routing_key
+     end
+
+     def perform
+       channel = connection.create_channel
+       exchange = channel.direct(exchange_name)
+       exchange.publish(message.to_json, routing_key: routing_key)
+       connection.close
+     end
+
+     private
+
+     attr_reader :message, :exchange_name, :routing_key
+
+     def connection
+       @connection ||= Bunny.new(connection_options).tap(&:start)
+     end
+
+     def connection_options
+       {
+         host: "localhost",
+         port: "5672",
+         vhost: "/",
+         username: "guest",
+         password: "guest"
+       }
+     end
+   end
+ end
